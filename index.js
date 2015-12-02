092f5b80853bfcda2c8df84847315ef0a4f35ebd
Abhinav Suraiya <absuraiya@gmail.com>
2015-12-01 19:34:13 -0800

    user connected event added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    io.emit('user connected', 'user connected')
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
