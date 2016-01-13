3d2eaae1e8abf02b1f1f99927af870b489f1bd93
Abhinav Suraiya <absuraiya@gmail.com>
2016-01-12 22:51:59 -0500

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app)
 var io = require('socket.io')(http);
+var port = process.env.PORT || 5000
 var userList = {};
 var typingTimeout;
 
@@ -37,7 +38,7 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
 
